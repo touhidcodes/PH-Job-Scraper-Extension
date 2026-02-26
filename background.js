979bf147110323a8e74c620b847945c310f6c524
Touhidur Zaman <touhidcodes@gmail.com>
2026-02-26 23:15:35 +0600

    mod: files

--- background.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let collectedJobs = [];
-chrome.runtime.onMessage.addListener((msg) => {
-    if (msg.type === "SCRAPED_JOBS") {
-        collectedJobs = [...collectedJobs, ...msg.payload];
-        chrome.storage.local.set({ jobs: collectedJobs });
-    }
-});
