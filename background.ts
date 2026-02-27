38904043e230b8af572ec22441de1304058e54e9
Touhidur Zaman <touhidcodes@gmail.com>
2026-02-27 22:52:28 +0600

    add: vite react tailwind

--- background.ts
+++
@@ -1,8 +0,0 @@
-let collectedJobs: any[] = [];
-
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.type === "SCRAPED_JOBS") {
-    collectedJobs = [...collectedJobs, ...msg.payload];
-    chrome.storage.local.set({ jobs: collectedJobs });
-  }
-});
--- types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface Job {
-  title: string;
-  company: string;
-  location: string;
-  url: string;
-  source: "LinkedIn" | "Bdjobs" | "Wellfound";
-}
